44f52127eb55e4b48dbc6fd48522de598d4d1ab5
Minkyu3 <gkhtls2006@broadwave.co.kr>
2021-06-17 11:55:10 +0900

    프로퍼티스변경 환경변수

--- src/test/java/com/broadwave/backend/NewDealBackendApplicationTest.java
+++ src/test/java/com/broadwave/backend/NewDealBackendApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class InfraBackendApplicationTest {
+public class NewDealBackendApplicationTest {
 
     @Test
     public void contextLoads() {
